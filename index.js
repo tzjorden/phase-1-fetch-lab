93c62b49141fb2fea0ffc6c58f7de2ae4fb4abdc
Tenzin Jorden <tenden96@gmail.com>
2022-01-04 12:11:46 -0800

    commit tree 0a43094d28b25e087775bcc57190ab650119bd05

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+function fetchBooks(){
+  fetch ("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())    //render the API's response as plain old JavaScript object
+  .then(json => renderBooks(json));  //the console.log() prints the JavaScript object to our console.
 }
 
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
+function renderBooks(books) {  
+  const main = document.querySelector('main') 
+    books.forEach(book => {   
+    const h2 = document.createElement('h2')    
+    h2.innerHTML = book.name    
+    main.appendChild(h2)  
+  })
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+
+document.addEventListener("DOMContentLoaded", function() {
   fetchBooks();
 });
+
